01d20baf93c348330c1cb25e00bd9bdaeaa7f912
Raphael Saldanha <rfsaldanha@gmail.com>
2024-11-20 15:48:44 +0100

    Remove feed url argument

--- R/parse_feed.R
+++ R/parse_feed.R
@@ -1,6 +1,5 @@
 #' Fetch and parse INMET alerts RSS feed
 #'
-#' @param feed_url character. INMET RSS feed. Defaults to current url.
 #' @param progress logical. Show progress bar.
 #'
 #' @return a tibble with alerts data
@@ -8,10 +7,9 @@
 #'
 #' @examples
 #' parse_feed()
-parse_feed <- function(feed_url = NULL, progress = TRUE){
-  if(is.null(feed_url)){
-    feed_url <- "https://apiprevmet3.inmet.gov.br/avisos/rss"
-  }
+parse_feed <- function(progress = TRUE){
+  # Feed URL
+  feed_url <- "https://apiprevmet3.inmet.gov.br/avisos/rss"
 
   # Fetch feed
   feed <- tidyRSS::tidyfeed(feed = feed_url)
